14a1366ac2538655a8f0c9f10b2bcefa8b104e14
nourssine02 <nourssinenef@gmail.com>
2024-09-16 17:37:14 +0100

    derniere modifications

---
+++ .history/frontend/src/App_20240916151438.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Connexion/Login';
+import Register from './components/Connexion/Register';
+import ProtectedRoutes from './components/Connexion/ProtectedRoutes';
+import { UserProvider } from './components/Connexion/UserProvider';
+import NotFoundPage from "./components/Connexion/Error Pages/NotFoundPage";
+import ForgotPassword from './components/Connexion/ForgotPassword';
+import ResetPassword from './components/Connexion/ResetPassword';
+import SelectionClient from './components/Dashboard/SelectionClient';
+const App = () => {
+  return (
+    <UserProvider>
+      <Router>  
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/selection-client" element={<SelectionClient />} />
+          <Route path="/*" element={<ProtectedRoutes />} />
+          <Route path="*" element={<NotFoundPage />} />
+           {/* Page 404 */}
+        <Route path="/404" element={<NotFoundPage />} />
+{/* Redirection vers 404 si aucune route ne correspond */}
+<Route path="*" element={<NotFoundPage />} />
+          <Route path="/forget_pass" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} /> 
+         
+        </Routes>
+      </Router>
+    </UserProvider>
+  );
+};
+
+export default App;
---
+++ .history/frontend/src/App_20240916172626.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Connexion/Login';
+import Register from './components/Connexion/Register';
+import ProtectedRoutes from './components/Connexion/ProtectedRoutes';
+import { UserProvider } from './components/Connexion/UserProvider';
+import NotFoundPage from "./components/Connexion/Error Pages/NotFoundPage";
+import ForgotPassword from './components/Connexion/ForgotPassword';
+import ResetPassword from './components/Connexion/ResetPassword';
+import SelectionClient from './components/Dashboard/SelectionClient';
+const App = () => {
+  return (
+    <UserProvider>
+      <Router>  
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/selection-client" element={<SelectionClient />} />
+          <Route path="/*" element={<ProtectedRoutes />} />
+          <Route path="/forget_pass" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} /> 
+         
+
+
+          <Route path="*" element={<NotFoundPage />} />
+
+        </Routes>
+      </Router>
+    </UserProvider>
+  );
+};
+
+export default App;
